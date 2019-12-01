f314ce00d0845aeca55492696d9a713910b46558
Keng.dev <songyos@wongnai.com>
2019-12-01 22:47:19 +0700

    Added endpoint for creating and authenticating user.

--- app/core/models.py
+++ app/core/models.py
@@ -15,7 +15,7 @@ class UserManager(BaseUserManager):
 
         return user
 
-    def create_super_user(self, email, password):
+    def create_superuser(self, email, password):
         """ Creates and save a new super user """
         user = self.create_user(email, password)
         user.is_staff = True
